b97fb941e09a69be1e4ad84a046b5f568ce6d24f
vstruk01 <stoliar444@gmail.com>
2020-12-16 14:52:31 +0200

    add breakout

--- Breakout/src/main.c
+++ Breakout/src/main.c
@@ -1,12 +1,18 @@
 #include "EndGame.h"
+#include <time.h>
+
 #define DELAY 3000000
+
 int main() {
     initscr();
     start_color();
-    enum e_direction direction_h = RIGHT;
-    enum e_direction direction_v = CENTER;
-    enum e_direction direction_r1 = NOT_MOVE;
-    // enum e_direction direction_r2 = NOT_MOVE;
+    // enum e_direction direction_h = CENTER;
+
+    enum e_direction direction_v = UP;
+
+    unsigned long speed = 60000;
+    clock_t start_t = clock();
+    clock_t end_t = 0;
 
     init_pair(1, COLOR_WHITE, COLOR_WHITE);
     init_pair(2, COLOR_WHITE, COLOR_BLACK);
@@ -22,17 +28,32 @@ int main() {
     int ball_x = 12, ball_y = 25;
 
     cbreak();
-    timeout(100);
-    wbkgd(stdscr, COLOR_PAIR(2));
+    timeout(0);
+    // wbkgd(stdscr, COLOR_PAIR(2));
     noecho();
     curs_set(FALSE);
+
     getmaxyx(stdscr, max_y, max_x);
     for (int i = 0; i < size_plat; ++i) {
-        mvprintw(y_plat, x_plat + i, "-");
         attron(COLOR_PAIR(1));
+        mvprintw(y_plat, x_plat + i, "-");
+        attroff(COLOR_PAIR(1));
     }
-    
-    
+
+    int color = 0;
+    int size_block = 8;
+    getmaxyx(stdscr, max_y, max_x);
+    for (; y_kir > 10; y_kir = y_kir - 2, x_kir = 0) {
+        for (;x_kir < max_x; x_kir++) {
+            if (x_kir % size_block) {
+                // attron(COLOR_PAIR(color));
+                mvprintw(y_kir, x_kir, "0");
+                // attroff(COLOR_PAIR(color));
+            } else {
+                color = rand() % 4 + 3;
+            }
+        }
+    }
     while (true) {
         switch (getch()) {
             case 68: //left
@@ -54,48 +75,28 @@ int main() {
                 }
                 break;
         }
-        getmaxyx(stdscr, max_y, max_x);
-        int color = 0;
+        mvwprintw(stdscr, ball_y, ball_x, " ");
+        if (direction_v == UP && ball_y - 1 <= 0) {
+            direction_v = DOWN;
+        } else if (direction_v == DOWN && ball_y + 1 >= getmaxy(stdscr)) {
+            exit(5);
+        } else if (direction_v == DOWN && ball_y + 1 == y_plat
+                   && ball_x >= x_plat && ball_x <= x_plat + size_plat) {
+            direction_v = UP;
+        }
 
-        for (; y_kir > 10; y_kir = y_kir - 2, x_kir = 0) {
-            for (;x_kir < max_x; x_kir++) {
-                if (x_kir % 8) {
-                    attron(COLOR_PAIR(color));
-                    mvprintw(y_kir, x_kir, " ");
-                    attroff(COLOR_PAIR(color));
-                }  
-                else {
-                    color = rand() % 4 + 3;
-                }
+        end_t = clock();
+        if (end_t - start_t > speed) {
+            start_t = end_t;
+            direction_v == UP ? --ball_y : ++ball_y;
+            if (mvinch(ball_y, ball_x) == '0') {
+                for (int i = ball_x - ball_x % size_block + 1; mvinch(ball_y, i) == '0'; ++i)
+                    mvprintw(ball_y, i, " ");
+                direction_v = DOWN;
+                ++ball_y;
             }
         }
-        mvwprintw(stdscr, ball_y, ball_x, " ");
-
-            // direction_r2 == UP? (direction_v = UP) : (direction_v = DOWN);
-            //     --ball_x;
-            // direction_h == RIGHT ? ++ball_x : --ball_x;
-            // if (direction_v == UP) {
-            //     ++ball_y;
-            // } 
-            // else if (direction_v == DOWN) {
-            //     --ball_y;
-            // }
-            if (ball_y >= y_kir && ball_y <= y_plat && ball_x == x_kir && direction_h == RIGHT) {
-                direction_h = LEFT;
-                direction_h == RIGHT ? ++ball_x : --ball_x;
-            } else if (ball_y >= y_kir && ball_y <= y_plat && ball_x == x_plat && direction_h == LEFT) {
-                direction_h = RIGHT;
-                direction_r1 == UP ? (direction_v = UP) : (direction_v = DOWN);
-                ++ball_x;
-            }
-            if (ball_x < 4 || ball_x >= getmaxx(stdscr) - 20) {
-                direction_v == RIGHT ? (direction_v = LEFT) : (direction_v = RIGHT);
-            }
-            else if (ball_y > 0 || ball_y < y_plat) {
-                // printf("%s","o");
-                direction_v == DOWN ? (direction_v = UP) : (direction_v);
-            }
-            mvwprintw(stdscr, ball_y, ball_x, "o");
+        mvwprintw(stdscr, ball_y, ball_x, "o");
         refresh();
     }
     endwin();
